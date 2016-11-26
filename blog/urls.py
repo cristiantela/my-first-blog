1e48560944eeef1740d15a28b163ae3dddc3a88b
Matheus Portela <mathues@email.com>
2016-11-26 18:02:08 -0200

    pronto

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.post_list),
+	url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='teste'),
+	url(r'^post/new/$', views.post_new_edit, name='post_new'),
+	url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_new_edit, name='post_edit'),
 ]
